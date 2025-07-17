00661a4d15e04a0a742904a0e74e48086b6fed8f
Loi Dang Ta <loikhuong1234@gmail.com>
2025-07-17 01:24:58 -0700

    add Assignment1.java

---
+++ Assignment1.java
@@ -0,0 +1,6 @@
+public class Assignment1 {
+    public static void main (String[] args ){
+        System.out.println("CSC142 -intro to Java");
+        System.out.print("I leanrned how to setup Java and praticing git pushing");
+    }
+}
