ad09810f8fae2393c747008200575913b3a36c42
Kune2707 <loikhuong1234@gmail.com>
2025-07-17 01:32:04 -0700

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -1,6 +1,6 @@
 public class Assignment1 {
     public static void main (String[] args ){
         System.out.println("CSC142 -intro to Java");
-        System.out.print("I leanrned how to setup Java and praticing git pushing");
+        System.out.print("I leanrned how to setup Java and practicing git pushing");
     }
 }
